6bdf0f6213e2edad2c85d248a680e0427a89728f
lixiaolong <lixiaolong@peicent.com>
2020-08-06 11:21:32 +0800

    结构调整

---
+++ video/src/main/java/com/lxl/video/controller/VideoController.java
@@ -0,0 +1,14 @@
+package com.lxl.video.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/video")
+public class VideoController {
+
+    @RequestMapping("/index.html")
+    public String video() {
+        return "index";
+    }
+}
---
+++ word/src/test/java/com/lxl/word/WordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxl.word;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
